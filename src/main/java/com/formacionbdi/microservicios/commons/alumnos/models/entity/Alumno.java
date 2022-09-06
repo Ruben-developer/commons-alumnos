b516c8dfa99c674e554518abef6a0e206f6a1d72
Ruben-developer <rcontreras.dev@protonmail.com>
2022-09-06 14:57:20 -0300

    Se añade atributo foto(blob) en entity Alumno

--- src/main/java/com/formacionbdi/microservicios/commons/alumnos/models/entity/Alumno.java
+++ src/main/java/com/formacionbdi/microservicios/commons/alumnos/models/entity/Alumno.java
@@ -7,12 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="alumnos")
@@ -35,10 +38,18 @@ public class Alumno {
 	@Column(name = "create_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
+
+	@Lob
+	@JsonIgnore
+	private byte[] foto;
 	
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
+	}
+
+	public Integer getFotoHashCode() {
+		return (this.foto != null) ? this.foto.hashCode(): null;
 	}
 
 	public Long getId() {
@@ -67,6 +78,14 @@ public class Alumno {
 
 	public String getEmail() {
 		return email;
+	}
+
+	public byte[] getFoto() {
+		return this.foto;
+	}
+
+	public void setFoto(byte[] foto) {
+		this.foto = foto;
 	}
 
 	public void setEmail(String email) {
